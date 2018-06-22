e93461b8c12d77c6cb6ebf0afa21d4faeb61124a
Igor Racki <A00248123@student.ait.ie>
2018-06-22 23:04:08 +0100

    fix another error

--- server/utils/users.js
+++ server/utils/users.js
@@ -1,3 +1,5 @@
+
+
 var colors = [
   'red', 'orange', 'green', 'blue', 'yellow'
 ];
@@ -32,7 +34,7 @@ class Users {
   }
   getUserList (room) {
     var users = this.users.filter((user) => user.room === room);
-    var namesArray = users.map((user) => {user.name});
+    var namesArray = users.map((user) => user.name);
 
     return namesArray;
   }
