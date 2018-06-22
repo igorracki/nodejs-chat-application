13992b7801ebf50270437a8cc199277246b0fb45
Igor Racki <igor.racki@ericsson.com>
2018-06-22 14:50:48 +0100

    init commit

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const express = require('express');
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+var app = express();
+
+app.use(express.static(publicPath));
+app.use((request, response, next) => {
+    var now = new Date().toString();
+    var log = '\x1b[1m \x1b[35m' + now + ': \x1b[33m' + request.method + ' \x1b[34m' + request.url + ' ' + ' \x1b[32m' + response.statusCode;
+    console.log(log);
+    next();
+});
+
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}.``);
+})
